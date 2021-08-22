75401661b5fa7ae5d7e4866b213df1aabe2a5d04
HMichaut <hectormichaut@gmail.com>
2021-08-22 13:43:07 +0200

    bug fixing

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,11 @@
 class StaticPagesController < ApplicationController
   def viewer
-    flickr = Flickr.new
-    photo_id_array = flickr.people.getPhotos(user_id: static_pages_params[:id]).map { |item| item.id }
-    info = photo_id_array.map { |id| flickr.photos.getInfo(:photo_id => id) }
-    @list = info.map { |item| Flickr.url_b(item) }
+    if static_pages_params.present?
+      flickr = Flickr.new
+      photo_id_array = flickr.people.getPhotos(user_id: static_pages_params[:id]).map { |item| item.id }
+      info = photo_id_array.map { |id| flickr.photos.getInfo(:photo_id => id) }
+      @list = info.map { |item| Flickr.url_b(item) }
+    end
   end
 
   private
